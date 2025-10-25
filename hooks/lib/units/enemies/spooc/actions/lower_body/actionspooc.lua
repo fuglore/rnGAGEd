1cd34cd7c55ba16be4c125d531fdfd0410a93d92
fugsystem <44823551+fuglore@users.noreply.github.com>
2025-10-25 13:38:06 -0300

    the clonker

--- hooks/lib/units/enemies/spooc/actions/lower_body/actionspooc.lua
+++ hooks/lib/units/enemies/spooc/actions/lower_body/actionspooc.lua
@@ -192,3 +192,100 @@ end
 function ActionSpooc:_chk_falling_behind()
 	return false
 end
+
+function ActionSpooc:sync_stop(pos, stop_nav_index)
+	if self._action_desc.flying_strike then
+		self:_expire()
+	else
+		if self._host_stop_pos_inserted then
+			stop_nav_index = stop_nav_index + self._host_stop_pos_inserted
+		end
+
+		local nav_path = self._nav_path
+
+		while stop_nav_index < #nav_path do
+			table.remove(nav_path)
+		end
+
+		self._stop_pos = pos
+
+		if #nav_path < stop_nav_index - 1 then
+			self._nr_expected_nav_points = stop_nav_index - #nav_path + 1
+		else
+			table.insert(nav_path, pos)
+		end
+
+		self._nav_index = math.min(self._nav_index, #nav_path - 1)
+
+		if self._end_of_path and not self._nr_expected_nav_points then
+			self._end_of_path = nil
+
+			self:_start_sprint()
+		end
+	end
+end
+
+function ActionSpooc:_upd_striking(t)
+	local target_unit = alive(self._strike_unit) and self._strike_unit or alive(self._target_unit) and self._target_unit
+	local my_pos = CopActionHurt._get_pos_clamped_to_graph(self, false)
+
+	if target_unit then
+		local my_fwd = self._common_data.fwd
+		local target_pos = target_unit:movement():m_pos()
+		local target_vec = ActionSpooc._tmp_vec1
+
+		mvector3.direction(target_vec, my_pos, target_pos)
+
+		if mvector3.dot(my_fwd, target_vec) < 0.98 then
+			local my_fwd_polar = my_fwd:to_polar_with_reference(target_vec, math.UP)
+			local spin_adj = math.step(0, -my_fwd_polar.spin, (self._ext_anim.spooc_enter and 180 or 110) * TimerManager:game():delta_time())
+
+			mvector3.set(target_vec, my_fwd)
+			mvector3.rotate_with(target_vec, Rotation(spin_adj, 0, 0))
+			self._ext_movement:set_rotation(Rotation(target_vec, math.UP))
+		end
+	end
+
+	self._ext_movement:upd_ground_ray(my_pos, true)
+
+	local gnd_z = self._common_data.gnd_ray.position.z
+
+	if gnd_z < my_pos.z then
+		self._last_vel_z = self._apply_freefall(my_pos, self._last_vel_z, gnd_z, TimerManager:game():delta_time())
+	else
+		if my_pos.z < gnd_z then
+			mvector3.set_z(my_pos, gnd_z)
+		end
+
+		self._last_vel_z = 0
+	end
+
+	self._ext_movement:set_position(my_pos)
+
+	if self._ext_anim.spooc_enter then
+		return
+	end
+	
+	if self._is_local and self._stop_pos and not Network:is_server() then
+		self:_expire()
+		
+		return
+	end
+
+	if self._is_local and not self._was_interrupted and (not target_unit or not target_unit:character_damage():is_downed()) then
+		if Network:is_server() then
+			self:_expire()
+		else
+			self._ext_network:send_to_host("action_spooc_stop", self._ext_movement:m_pos(), 1, self._action_id)
+			self:_wait()
+		end
+
+		return
+	end
+
+	if not self._taunt_at_beating_played then
+		self._taunt_at_beating_played = true
+
+		self._unit:sound():say(self._taunt_during_assault, nil, true)
+	end
+end
